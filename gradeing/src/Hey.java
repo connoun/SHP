a9cdc589324448a5981ce50596b15aef846c876f
pronoun <apronoun@gmail.com>
2014-10-18 11:05:15 -0400

    check it out :DD

--- gradeing/src/Hey.java
+++ gradeing/src/Hey.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class Hey {
 	public static void main(String[] args) {
 		String babby = "Kevin Li";
 		System.out.println("Best bf everrrrr: " + babby);
+		System.out.println("How cute are you?");
+		Scanner s = new Scanner(System.in);
+		String cute = s.nextLine();
+		for (int i=0; i<cute.length(); i++) {
+			System.out.println(cute.charAt(i));}
+String indeed = "indeed";
+System.out.println(" ");
+		for (int k=0; k<indeed.length();k++){
+			System.out.println(indeed.charAt(k));}
+		}
 	}
+
 
 
 
